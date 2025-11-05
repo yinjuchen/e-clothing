734e67ee5dfe3158899e9ecb58315ffd4552d5fe
tiffanychen <tiffanyc1213@gmail.com>
2025-11-05 19:51:35 +0800

    added sign in component, firebase utils and brought in firebase libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,23 @@
-import './App.css';
-import './categories.styles.scss'
-import Directory from './components/directory/directory.component';
+import {Routes, Route} from 'react-router-dom'
+import Navigation from './routes/navigaion/navigation.component';
+
+import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => {
+  return <h1>I am at the Shop page</h1>
+}
 
 const App =()=> {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imageUrl:'https://i.ibb.co/cvpntL1/hats.png'
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imageUrl:'https://i.ibb.co/px2tCc3/jackets.png'
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png'
-
-    },
-    {
-      id: 4,
-      title: 'Womens',
-      imageUrl:'https://i.ibb.co/GCCdy8t/womens.png'
-    },
-    {
-      id: 5,
-      title:'Mens',
-      imageUrl:'https://i.ibb.co/R70vBrQ/men.png'
-    }
-  ]
   return (
-    <div>
-     <Directory categories={categories}/>
-    </div>
-  );
+  <Routes>
+    <Route path='/' element={<Navigation/>}>
+      <Route index element={<Home/>}/>
+      <Route path='/shop' element={<Shop/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+    </Route>
+  </Routes>
+  )
 }
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,76 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBaeBFDWZuFJySaG-zb6Bkt8TkTg1nTHo",
+  authDomain: "e-clothing-2ce58.firebaseapp.com",
+  projectId: "e-clothing-2ce58",
+  storageBucket: "e-clothing-2ce58.firebasestorage.app",
+  messagingSenderId: "987732067785",
+  appId: "1:987732067785:web:9d282d070ffd6655b5d418",
+  measurementId: "G-EEZ3LFV26W"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  console.log(userAuth);
+};
+
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore()
+
+// Create a Function to Store User Data
+export const createUserDocumentFromAuth = async (userAuth) => {
+  //If userAuth does not exist, then stop running the rest of this function
+  if (!userAuth) return
+
+  // Reference to the user document inside "users" collection
+  const userDocRef = doc(db, 'users', userAuth.uid)
+  console.log(userDocRef)
+
+  // Check if the user already exists
+  const userSnapshot = await getDoc(userDocRef)
+  console.log(userSnapshot)
+  console.log(userSnapshot.exists())
+
+  // If not, create a new document
+  if(!userSnapshot.exists()) {
+    const {displayName, email} = userAuth
+    const createdAt = new Date()
+  
+    try {
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createdAt
+      })
+    } catch(error) {
+      console.log('error cretating the user',error.message)
+    }
+  }
+  return userDocRef
+}
